b932f8ce4f8c8605fea290de908c6a10cc38aca9
molinwu <858459615@qq.com>
2024-07-05 23:24:39 +0800

    update

--- Student_platform.Server/Competition.cs
+++ Student_platform.Server/Competition.cs
@@ -7,11 +7,33 @@ namespace Student_platform.Server
 {
     public class Competition
     {
-        public string cmpt_type {  get; set; }
+        public string cmpt_type { get; set; }
 
         public string cmpt_name { get; set; }
 
-        public 
+        public string cmpt_date { get; set; }
+
+        public string cmpt_description { get; set; }
+
+        public string cmpt_teacher_advise { get; set; }
+
+        public string Show()
+        {
+            Competition cmpt = new Competition
+            {
+                cmpt_type = this.cmpt_type,
+                cmpt_date = this.cmpt_date,
+                cmpt_description = this.cmpt_description,
+                cmpt_name = this.cmpt_name,
+                cmpt_teacher_advise = this.cmpt_teacher_advise
+            };
+            string json = JsonSerializer.Serialize(cmpt);
+            return json;
+        }
+
+
+
+
 
     }
 }
