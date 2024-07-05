b5b9f4d31a3b6f384520e4f7c72b22683b202498
无畏 <13917350+fearlessman@user.noreply.gitee.com>
2024-07-05 19:29:11 +0800

    7.5

---
+++ vueapp1.client/src/components/home/calendar.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="calendar">
+        <div class="cal-title">Time</div>
+        <el-calendar>
+        </el-calendar>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    })
+}
+</script>
+
+<style lang="less">
+.calendar {
+    width: 380px;
+    height: 430px;
+    // overflow: hidden;
+    // top: 200px;
+    // position: absolute;
+}
+
+.el-calendar__header{
+    width: 300px;
+    height: 25px;
+    // border: 1px red solid;
+    background-color: #ffffff5b;
+    border-radius: 20px 20px 0 0;
+    box-shadow: 2px 2px 10px #dadada;
+    position: absolute;
+    top: 20px;
+    left: 20px;
+    z-index: 0;
+}
+
+.el-calendar{
+    border-radius: 10px;
+}
+
+.el-calendar__body {
+    background-color: #ffffff5b;
+    box-shadow: 5px 5px 10px #dadada;
+    border-radius: 0 0 20px 20px;
+    width: 300px;
+    position: absolute;
+    top: 70px;
+    left: 20px;
+    z-index: 1;
+}
+
+.el-calendar-table .el-calendar-day {
+    // border-radius: 10px;
+    height: 40px;
+}
+
+.el-button-group>.el-button:first-child{
+    // width: 50px;
+    text-align: center;
+}
+.cal-title{
+    position: absolute;
+    top: -12px;
+    left: 35px;
+    font-size: 18px;
+    color: #979797;
+    cursor: default;
+}
+</style>
---
+++ vueapp1.client/src/components/login_page/login_page.vue
@@ -0,0 +1,54 @@
+<template>
+    <!-- 登录表单 -->
+        <el-card class="login-card">
+          <h3 class="login-title">登录</h3>
+          <el-form :model="loginForm" :rules="rules" ref="loginForm" label-width="100px">
+            <el-form-item label="用户名" prop="username">
+              <el-input v-model="loginForm.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+              <el-input type="password" v-model="loginForm.password"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm('loginForm')">登录</el-button>
+              <el-button @click="getRegisterPage()">注册</el-button>
+            </el-form-item>
+          </el-form>
+        </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 15, message: '长度在 3 到 15 个字符', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, message: '密码长度不能少于6位', trigger: 'blur' }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('提交登录信息!');
+          // 这里可以添加登录的逻辑
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
