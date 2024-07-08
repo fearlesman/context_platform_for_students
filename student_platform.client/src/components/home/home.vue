493edb37716916a48da065045e7f5fb9b2376f1c
data740 <3073983973@qq.com>
2024-07-08 16:11:38 +0800

    000

--- student_platform.client/src/components/home/home.vue
+++ student_platform.client/src/components/home/home.vue
@@ -33,45 +33,29 @@ export default {
   },
 };
 </script>
+
 <template>
-    <el-row>
-        <!-- 走马灯 -->
-        <!-- 右侧日历 -->
-        <el-col :span="12">
-            <div class="calendar-container">
-                <el-calendar v-loading="loading">
-                  <template #dateCell="{data}">
-                    <div
-                      class="custom-date"
-                      @mouseenter="handleMouseEnter(data.date)"
-                      @mouseleave="handleMouseLeave"
-                    >
-                      {{ data.day.split('-').slice(-1)[0] }}
-                    </div>
-                  </template>
-                </el-calendar>
-                <el-popover 
-                  v-model:visible="isPopoverVisible"
-                  placement="bottom"
-                  width="300"
-                >
-                  <p>详细信息: {{ selectedDateInfo }}</p>
-                  <template v-slot:reference>
-                    <div class="trigger-area" ></div>
-                  </template>
-                </el-popover>
-              </div>
-        </el-col>
-        <el-col :span="12">
-            <el-carousel :interval="5000" arrow="always">
-                <el-carousel-item v-for="item in content_pages" :key="item">
-                <h3 text="2xl" justify="center">{{ item }}</h3>
-                </el-carousel-item>
-            </el-carousel>
-        </el-col>
-    </el-row>
+    <el-carousel :interval="4000" type="card" height="250px">
+        <el-carousel-item v-for="item in 6" :key="item">
+            <h3 text="2xl" justify="center">{{ item }}</h3>
+        </el-carousel-item>
+    </el-carousel>
 </template>
 
-<style>
+<style scoped>
+    .el-carousel__item h3 {
+        color: #475669;
+        opacity: 0.75;
+        line-height: 200px;
+        margin: 0;
+        text-align: center;
+    }
 
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n + 1) {
+        background-color: #d3dce6;
+    }
 </style>
