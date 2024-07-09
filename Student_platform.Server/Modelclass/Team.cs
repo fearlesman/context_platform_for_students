b0224b715ff94c9c3d0325d0fb82edf745e157ff
molinwu <858459615@qq.com>
2024-07-09 10:53:46 +0800

    update

---
+++ Student_platform.Server/Modelclass/Team.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Numerics;
+using System.Text.Json;
+
+
+namespace Student_platform.Server.Modelclass
+{
+
+    public class Team_member
+    {
+        public string name { get; set; }
+
+        public string id { get; set; }
+
+        public string avatar { get; set; }
+
+    }
+
+
+
+    public class Team
+    {
+        public string name { get; set; }
+
+        public string leaderid { get; set; }
+
+        public string leaderName { get; set; }
+
+        public string leaderAvatar { get; set; }
+
+        public Team_member[] members { get; set; }
+
+        public string race { get; set; }
+
+        public string description { get; set; }
+
+        public string type { get; set; }
+
+        public int currentMembers { get; set; }
+
+        public int totalMembers { get; set; }
+
+        public string status { get; set; }
+
+        public string[] tags { get; set; }
+
+        
+
+     
+    }
+}
