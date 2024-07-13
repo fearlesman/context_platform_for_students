f15397d10d470522c86bd369a21aa1774249ede9
无畏 <13917350+fearlessman@user.noreply.gitee.com>
2024-07-13 23:09:49 +0800

    modified:   src/rounter/index.js

--- student_platform.client/src/rounter/index.js
+++ student_platform.client/src/rounter/index.js
@@ -13,17 +13,53 @@ const routes = [
     component: contest_page ,
     name:'join',
     beforeEnter: (to, from, next) => {
-      const profileComponent = to.matched[0].instances.default;
-      profileComponent.searchData();
-      next();
+      if(this.$store.state.user.id!='')
+        next()
+      else
+        next('/')
     }
   },
   { path: '/login', component: login_page ,name:'login'},
   { path: '/register', component: register_page,name:'register'},
-  { path: '/user/:id/markdown', component: markdown_page,name:'markdown'},
-  { path: '/user/:id/create', component: create_page,name:'create'},
-  { path: '/user/:id/home', component: personal_page,name:'home'},
-  { path: '/user/:id/myteam', component: myteam_page,name:'myteam'},
+  { path: '/user/:id/markdown', 
+    component: markdown_page,
+    name:'markdown',
+    beforeEnter: (to, from, next) => {
+      if(this.$store.state.user.id!='')
+        next()
+      else
+        next('/')
+    }},
+  { path: '/user/:id/create', 
+    component: create_page,
+    name:'create',
+    beforeEnter: (to, from, next) => {
+      if(this.$store.state.user.id!='')
+        next()
+      else
+        next('/')
+    }
+  },
+  { path: '/user/:id/home', 
+    component: personal_page,
+    name:'home',
+    beforeEnter: (to, from, next) => {
+      if(this.$store.state.user.id!='')
+        next()
+      else
+        next('/')
+    }
+  },
+  { path: '/user/:id/myteam', 
+    component: myteam_page,
+    name:'myteam',
+    beforeEnter: (to, from, next) => {
+      if(this.$store.state.user.id!='')
+        next()
+      else
+        next('/')
+    }
+  },
   { path: '/home', redirect: '/user/:id/home'}
 ]
 
